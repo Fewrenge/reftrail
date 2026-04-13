9dfb2bf6b35b07415f0666114433562c79e5488c
Fung <szechungfung@outlook.com>
2026-04-13 12:46:30 -0400

    leave only one UserContext definition

--- server/auth/context.go
+++ server/auth/context.go
@@ -1,18 +1,8 @@
 package auth
 
-import (
-	"wl/internal/types"
-)
-
 // ContextKey is a custom type to prevent name collisions
 type ContextKey string
 
 const (
 	UserContextKey ContextKey = "user"
 )
-
-// UserContext holds the data we actually care about during a request
-type UserContext struct {
-	ID   types.UserID
-	Role types.UserRole
-}
